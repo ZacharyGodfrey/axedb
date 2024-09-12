03fad1038a5828518b783d7d79a67a0286de09a6
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2024-09-12 15:43:20 -0400

    WIP

--- scrape/app.js
+++ scrape/app.js
@@ -1,3 +1,5 @@
+// Helpers
+
 const reactPageState = (page, selector) => {
   return page.$eval(selector, (element) => {
     return element._reactRootContainer._internalRoot.current.memoizedState.element.props.store.getState();
@@ -12,7 +14,19 @@ const isDesiredResponse = (method, status, url) => {
   };
 };
 
-export const enums = {
+const waitMilliseconds = (milliseconds) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+};
+
+const round = (places, value) => {
+  const factor = 10 ** places;
+
+  return Math.round(value * factor) / factor;
+};
+
+const enums = {
   tool: {
     hatchet: 'hatchet',
     bigAxe: 'big axe'
@@ -22,6 +36,8 @@ export const enums = {
     clutch: 'clutch'
   }
 };
+
+// Retrieve Data
 
 export const getProfileImage = async (profileId) => {
   const url = `https://admin.axescores.com/pic/${profileId}`;
@@ -62,31 +78,34 @@ export const getMatchData = async (page, profileId, matchId) => {
 
   const opponentId = rawMatch.players.find(x => x.id !== profileId)?.id ?? 0;
 
-  rawMatch.rounds.forEach(({ order: roundNumber, name, games }) => {
+  for (const { order: roundNumber, name, games } of rawMatch.rounds) {
     const game = games.find(x => x.player === profileId);
 
     if (!game || game.forfeit === true) {
-      return;
+      continue;
     }
 
     const isBigAxe = name === 'Tie Break';
     const { Axes: axes = [] } = game;
 
-    axes.forEach(({ order: throwNumber, score, clutchCalled: isClutch }) => {
+    for (const { order: throwNumber, score, clutchCalled: isClutch } of axes) {
       throws.push({
         profileId,
+        opponentId,
         matchId,
-        round: roundNumber,
-        throw: throwNumber,
+        roundId: roundNumber,
+        throwId: throwNumber,
         tool: isBigAxe ? enums.tool.bigAxe : enums.tool.hatchet,
         target: isClutch ? enums.target.clutch : enums.target.bullseye,
         score
       });
-    });
-  });
+    }
+  }
 
-  return { opponentId, throws };
+  return throws;
 };
+
+// Process Data
 
 export const getStats = (throws) => {
   const result = {
@@ -179,14 +198,218 @@ export const getStats = (throws) => {
   return result;
 };
 
-export const waitMilliseconds = (milliseconds) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, milliseconds);
-  });
+export const groupBy = (items, property) => {
+  const lookup = {};
+
+  for (const item of items) {
+    const key = item[property] ?? 'undefined';
+
+    lookup[key] = lookup[key] ?? [];
+    lookup[key].push(item);
+  }
+
+  return lookup;
 };
 
-export const round = (places, value) => {
-  const factor = 10 ** places;
+export const buildHierarchy = (throws) => {
+  const profiles = {};
 
-  return Math.round(value * factor) / factor;
+  for (const { profileId, seasonId, weekId, matchId, roundId, throwId, tool, target, score } of throws) {
+    const throwData = { throwId, tool, target, score };
+
+    const { seasons } = profiles[profileId] = profiles[profileId] ?? { profileId, seasons: {}, throws: [] };
+    const { weeks } = seasons[seasonId] = seasons[seasonId] ?? { seasonId, weeks: {}, throws: [] };
+    const { matches } = weeks[weekId] = weeks[weekId] ?? { weekId, matches: {}, throws: [] };
+    const { rounds } = matches[matchId] = matches[matchId] ?? { matchId, rounds: {}, throws: [] };
+    const round = rounds[roundId] = rounds[roundId] ?? { roundId, throws: [] };
+
+    const profile = profiles[profileId];
+    profile.throws.push(throwData);
+
+    const season = profile.seasons[seasonId];
+    season.throws.push(throwData);
+
+    const week = season.weeks[weekId];
+    week.throws.push(throwData);
+
+    const match = week.matches[matchId];
+    match.throws.push(throwData);
+
+    const round = match.rounds[roundId];
+    round.throws.push(throwData);
+  }
+
+  return Object.values(profiles);
+};
+
+export const aggregationGroups = (throws) => {
+  const groups = {};
+
+  for (const { profileId, seasonId, weekId, matchId, roundId, tool, target, score } of throws) {
+    const profilePath = `p${profileId}`;
+    const seasonPath =  `p${profileId}s${seasonId}`;
+    const weekPath =    `p${profileId}s${seasonId}w${weekId}`;
+    const matchPath =   `p${profileId}m${matchId}`;
+    const roundPath =   `p${profileId}m${matchId}r${roundId}`;
+    const data = { tool, target, score };
+
+    for (const entityPath of [profilePath, seasonPath, weekPath, matchPath, roundPath]) {
+      const { throws } = groups[entityPath] = groups[entityPath] ?? { entityPath, throws: [] };
+
+      throws.push(data);
+    }
+  }
+
+  return Object.values(groups);
+};
+
+// Write Data
+
+export const seedProfiles = (db, profileIds) => {
+  for (const profileId of profileIds) {
+    db.main.run(`
+      INSERT INTO profiles (profileId, fetch)
+      VALUES (:profileId, 1)
+    `, { profileId });
+  }
+};
+
+export const createAggregation = (db, entityPath, throws) => {
+  const { hatchet, bigAxe } = getStats(throws);
+  const stats = {
+    hatchetBullseyeHitPercent: hatchet.bullseye.hitPercent,
+    hatchetBullseyeScorePerAxe: hatchet.bullseye.scorePerAxe,
+    hatchetClutchHitPercent: hatchet.clutch.hitPercent,
+    hatchetClutchScorePerAxe: hatchet.clutch.scorePerAxe,
+    hatchetClutchFiveHitPercent: hatchet.clutch.fiveHitPercent,
+    hatchetClutchSevenHitPercent: hatchet.clutch.sevenHitPercent,
+    bigAxeBullseyeHitPercent: bigAxe.bullseye.hitPercent,
+    bigAxeBullseyeScorePerAxe: bigAxe.bullseye.scorePerAxe,
+    bigAxeClutchHitPercent: bigAxe.clutch.hitPercent,
+    bigAxeClutchScorePerAxe: bigAxe.clutch.scorePerAxe,
+    bigAxeClutchFiveHitPercent: bigAxe.clutch.fiveHitPercent,
+    bigAxeClutchSevenHitPercent: bigAxe.clutch.sevenHitPercent,
+  };
+
+  const create = { entityPath, ...stats };
+  const conflict = { entityPath };
+  const update = stats;
+
+  db.stats.insertOrUpdate('stats', create, conflict, update);
+};
+
+// Workflow
+
+export const fetchMainData = async (db, page, profiles) => {
+  for (const { profileId } of profiles) {
+    console.log(`Profile ${profileId}`);
+
+    const [image, { name, leagues }] = await Promise.all([
+      getProfileImage(profileId),
+      getPlayerData(page, profileId)
+    ]);
+
+    db.main.run(`
+      UPDATE profiles
+      SET name = :name, image = :image
+      WHERE profileId = :profileId
+    `, { profileId, name, image });
+
+    for (const { id: seasonId, seasonWeeks, performanceName, ...season } of leagues) {
+      if (performanceName !== RULESET) {
+        continue;
+      }
+
+      console.log(`Season ${seasonId}`);
+
+      const name = `${season.name} ${season.shortName}`;
+      const year = parseInt(season.date.split('-')[0]);
+
+      db.main.run(`
+        INSERT INTO seasons (seasonId, name, year)
+        VALUES (:seasonId, :name, :year)
+        ON CONFLICT (seasonId) DO UPDATE
+        SET name = :name, year = :year
+      `, { seasonId, name, year });
+
+      for (const { week, matches } of seasonWeeks) {
+        console.log(`Week ${week}`);
+
+        for (const { id: matchId } of matches) {
+          console.log(`Match ${matchId}`);
+
+          db.main.run(`
+            INSERT INTO matches (profileId, seasonId, weekId, matchId)
+            VALUES (:profileId, :seasonId, :week, :matchId)
+            ON CONFLICT (profileId, matchId) DO UPDATE
+            SET week = :week
+          `, { profileId, seasonId, week, matchId });
+        }
+      }
+    }
+  }
+};
+
+export const fetchThrowData = async (db, page, newMatches) => {
+  for (const { profileId, seasonId, weekId, matchId } of newMatches) {
+    console.log(`Match ${matchId}`);
+
+    const throws = await getThrowData(page, profileId, matchId);
+
+    console.table(throws);
+
+    for (const row of throws) {
+      db.throws.insert('throws', { ...row, seasonId, weekId });
+    }
+
+    db.main.run(`
+      UPDATE matches
+      SET processed = 1, opponentId = :opponentId
+      WHERE profileId = :profileId AND matchId = :matchId
+    `, { profileId, opponentId, matchId });
+  }
+};
+
+export const fetchOpponents = async (db, page) => {
+  const opponents = db.main.rows(`
+    SELECT DISTINCT opponentId FROM matches
+    WHERE profileId NOT IN (
+      SELECT profileId FROM profiles
+      WHERE fetch = 1
+    )
+  `);
+
+  for (const { profileId } of opponents) {
+    const [image, { name }] = await Promise.all([
+      getProfileImage(profileId),
+      getPlayerData(page, profileId)
+    ]);
+
+    const create = { profileId, name, image };
+    const conflict = { profileId };
+    const update = { name, image };
+
+    db.main.insertOrUpdate('profiles', create, conflict, update);
+  }
+};
+
+export const buildStats = (db, profiles) => {
+  for (const { profileId } of profiles) {
+    console.log(`Profile ${profileId}`);
+
+    const allThrows = db.throws.rows(`
+      SELECT * FROM throws
+      WHERE profileId = :profileId
+    `, { profileId });
+
+    console.log(`Found ${allThrows.length} throws...`);
+
+    const groups = aggregationGroups(allThrows);
+
+    for (const { entityPath, throws } of groups) {
+      console.log(`Stats ${entityPath}`);
+
+      createAggregation(db, entityPath, throws);
+    }
+  }
 };
--- scrape/index.js
+++ scrape/index.js
@@ -1,43 +1,18 @@
 import puppeteer from 'puppeteer';
 
 import { database } from '../lib/database.js';
-import { getProfileImage, getPlayerData, getMatchData, getStats } from './app.js';
-
-const createAggregation = (db, name, level, throws) => {
-  const { hatchet, bigAxe } = getStats(throws);
-  const stats = {
-    hatchetBullseyeHitPercent: hatchet.bullseye.hitPercent,
-    hatchetBullseyeScorePerAxe: hatchet.bullseye.scorePerAxe,
-    hatchetClutchHitPercent: hatchet.clutch.hitPercent,
-    hatchetClutchScorePerAxe: hatchet.clutch.scorePerAxe,
-    hatchetClutchFiveHitPercent: hatchet.clutch.fiveHitPercent,
-    hatchetClutchSevenHitPercent: hatchet.clutch.sevenHitPercent,
-    bigAxeBullseyeHitPercent: bigAxe.bullseye.hitPercent,
-    bigAxeBullseyeScorePerAxe: bigAxe.bullseye.scorePerAxe,
-    bigAxeClutchHitPercent: bigAxe.clutch.hitPercent,
-    bigAxeClutchScorePerAxe: bigAxe.clutch.scorePerAxe,
-    bigAxeClutchFiveHitPercent: bigAxe.clutch.fiveHitPercent,
-    bigAxeClutchSevenHitPercent: bigAxe.clutch.sevenHitPercent,
-  };
-
-  const table = 'aggregations';
-  const create = { name, level, ...stats };
-  const conflict = { name, level };
-  const update = stats;
-
-  db.upsert(table, create, conflict, update);
-};
+import { seedProfiles, fetchMainData, fetchThrowData, fetchOpponents, buildStats } from './app.js';
 
 // Start Up
 
 console.log('Starting up...');
 
 const START = Date.now();
-const DB_FILE = 'database.db';
+const DATA_DIR = 'data';
 const PROFILE_ID = 1207260;
-const MATCH_TYPE = 'IATF Premier';
+const RULESET = 'IATF Premier';
 
-const db = database(DB_FILE);
+const db = database(DATA_DIR);
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
@@ -45,72 +20,22 @@ const page = await browser.newPage();
 
 console.log('Seeding profiles...');
 
-db.insertOrIgnore('profiles', {
-  profileId: PROFILE_ID,
-  fetch: 1
-});
+seedProfiles(db, [PROFILE_ID]);
 
 console.log('Done.');
 
-// Fetch profile data
+// Fetch main data
 
-console.log('Fetching profile data...');
+console.log('Fetching main data...');
 
-const profiles = db.rows(`
-  SELECT profileId
-  FROM profiles
+const profiles = db.main.rows(`
+  SELECT profileId FROM profiles
   WHERE fetch = 1
 `);
 
-for (const { profileId } of profiles) {
-  console.log(`Profile ${profileId}`);
+console.log(`Processing ${profiles.length} profiles...`);
 
-  const [image, playerData] = await Promise.all([
-    getProfileImage(profileId),
-    getPlayerData(profileId)
-  ]);
-
-  db.run(`
-    UPDATE profiles
-    SET name = :name, image = :image
-    WHERE profileId = :profileId
-  `, { profileId, name: playerData.name, image });
-
-  for (const season of playerData.leagues) {
-    const { id: seasonId, seasonWeeks, performanceName } = season;
-
-    if (performanceName !== MATCH_TYPE) {
-      continue;
-    }
-
-    console.log(`Season ${seasonId}`);
-
-    const seasonName = `${season.name} ${season.shortName}`;
-    const seasonYear = parseInt(season.date.split('-')[0]);
-
-    db.run(`
-      INSERT INTO seasons (seasonId, name, year)
-      VALUES (:seasonId, :seasonName, :seasonYear)
-      ON CONFLICT (seasonId) DO UPDATE
-      SET name = :seasonName, year = :seasonYear
-    `, { seasonId, seasonName, seasonYear });
-
-    for (const { week, matches } of seasonWeeks) {
-      console.log(`Week ${week}`);
-
-      for (const { id: matchId } of matches) {
-        console.log(`Match ${matchId}`);
-
-        db.run(`
-          INSERT INTO matches (profileId, seasonId, week, matchId)
-          VALUES (:profileId, :seasonId, :week, :matchId)
-          ON CONFLICT (profileId, matchId) DO UPDATE
-          SET week = :week
-        `, { profileId, seasonId, week, matchId });
-      }
-    }
-  }
-}
+await fetchMainData(page, profiles);
 
 console.log('Done.');
 
@@ -118,68 +43,31 @@ console.log('Done.');
 
 console.log('Fetching throw data...');
 
-const newMatches = db.rows(`SELECT * FROM matches WHERE processed = 0`);
+const newMatches = db.main.rows(`
+  SELECT profileId, seasonId, weekId, matchId
+  FROM matches
+  WHERE processed = 0
+`);
 
 console.log(`Processing ${newMatches.length} new matches...`);
 
-for (const { profileId, matchId } of newMatches) {
-  console.log(`Match ${matchId}`);
-
-  const { opponentId, throws } = await getMatchData(page, profileId, matchId);
-
-  console.table(throws);
-
-  throws.forEach((row) => db.insert('throws', row));
-
-  db.run(`
-    UPDATE matches
-    SET processed = 1, opponentId = :opponentId
-    WHERE matchId = :matchId
-  `, { profileId, opponentId });
-}
+await fetchThrowData(page, newMatches);
 
 console.log('Done.');
 
-// Reset aggregations
+// Fetch Opponents
 
-console.log('Resetting aggregations...');
+console.log('Fetching opponent data...');
 
-db.run('DELETE FROM aggregations');
-
-console.log('Done.');
-
-// Career stats
-
-console.log('Aggregating career stats...');
-
-createAggregation(db, 'Career', 'career', db.rows(`
-  SELECT *
-  FROM throws
-  ORDER BY seasonId, week, matchId, round, throw
-`));
+await fetchOpponents(db, page);
 
 console.log('Done.');
 
-// Season stats
+// Build profile stats
 
-console.log('Aggregating season stats...');
+console.log('Building stats...');
 
-const seasonIds = db.rows(`
-  SELECT DISTINCT seasonId
-  FROM throws
-  ORDER BY seasonId ASC
-`);
-
-seasonIds.forEach(({ seasonId }, index) => {
-  console.log(`Season: ${seasonId}`);
-
-  createAggregation(db, `Season #${index + 1} (${seasonId})`, 'season', db.rows(`
-    SELECT *
-    FROM throws
-    WHERE seasonId = ?
-    ORDER BY seasonId, week, matchId, round, throw
-  `, [seasonId]));
-});
+buildStats(db, profiles);
 
 console.log('Done.');
 
@@ -189,7 +77,7 @@ console.log('Tearing down...');
 
 await browser.close();
 
-db.run('VACUUM');
+db.shrink();
 
 console.log('Done.');
 console.log(`Total Runtime: ${Date.now() - START}ms`);
